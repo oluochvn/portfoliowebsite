d603b98b2cfcf8d08a86ea36cc9d59b40384a1a9
oluochvn <red.oluoch@gmail.com>
2025-11-07 00:18:05 +0300

    Add catch-all route to serve index.html
    Added a catch-all route to serve index.html for unmatched requests.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import { Contact } from './schema/contact.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
@@ -21,16 +22,14 @@ app.get('/test', (req, res) => {
   res.send('working');
 });
 
-app.post('/submit', async (req, res) => {
-  try {
-    const { name, email, message } = req.body;
-    const newContact = new Contact({ name, email, message });
-    await newContact.save();
-    res.send("your message has been received, we'll get in contact soon");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Server error");
-  }
+// 👉 Add this route at the bottom
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 export default app;
