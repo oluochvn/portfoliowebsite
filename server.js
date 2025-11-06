12045249ec131e4330881e42c883b1b550761257
oluochvn <red.oluoch@gmail.com>
2025-11-07 00:03:36 +0300

    Refactor server.js with improved error handling
    Added error handling for the POST /submit route.

--- server.js
+++ server.js
@@ -4,34 +4,33 @@ import bodyParser from 'body-parser';
 import { Contact } from './schema/contact.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import {body,validationResult} from 'express-validator'
+
+dotenv.config();
 
 const app = express();
-dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.atlas_url)
+  .then(() => console.log('connected to the database'))
+  .catch((err) => console.log(err));
 
-    .then(()=> console.log('connected to the database'))
-    .catch((err)=>console.log(err))
-app.get('/test',(req,res)=>{
-    res.send('working')
-})
-app.post('/submit',async (req,res)=>{
-    try{
-        const {name,email,message} = req.body;
-        const newContact =  new Contact({
-            name, email ,message,
-        })
-   await newContact.save();
-    res.send("your message has been received, we'll get in contact soon")
-    }
-    catch(err){
-        console.log(err)
-    }
+app.get('/test', (req, res) => {
+  res.send('working');
+});
 
-})
-app.listen(3000, ()=> console.log('running on port 3000'))
+app.post('/submit', async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    const newContact = new Contact({ name, email, message });
+    await newContact.save();
+    res.send("your message has been received, we'll get in contact soon");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server error");
+  }
+});
+
+export default app;
